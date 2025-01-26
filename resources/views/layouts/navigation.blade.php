66e2e6ad84895bc48e1485a999ecf95b68f37bbb
Yuyuucho <choyu32specium@gmail.com>
2025-01-26 00:27:41 +0000

    CSS   CSS適応étekiou v-1

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -33,13 +33,15 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
+                <x-dropdown align="right" width="48">                
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                        @auth
-                        <div>{{ Auth::user()->name }}</div>
-                        @endauth
-
+                            @auth    
+                            <div>{{ Auth::user()->name }}</div>
+                            @endauth
+                            @guest
+                            <div>guest</div>
+                            @endguest
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -50,10 +52,13 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
+                            @auth
                             {{ __('Profile') }}
+                            @endauth
                         </x-dropdown-link>
 
                         <!-- Authentication -->
+                         @auth
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
@@ -63,6 +68,23 @@
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
+                        @endauth
+                        @guest
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('login')">
+                                {{ __('Log In') }}
+                            </x-dropdown-link>
+                        </form>
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('register')">
+                                {{ __('Sign Up') }}
+                            </x-dropdown-link>
+                        </form>
+                        @endguest
                     </x-slot>
                 </x-dropdown>
             </div>
