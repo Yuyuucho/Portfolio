95f45e8bb1031c0517044d72a6cd927c2f34d016
Yuyuucho <choyu32specium@gmail.com>
2025-02-11 12:50:27 +0000

    implemented css

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -3,57 +3,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Build</title>
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>RE:SCHOOL</title>
+    <link href="https://fonts.googleapis.com/css?family=DotGothic16&family=Nunito:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/create.css') }}">
 </head>
 <x-app-layout>
     <body>
-        <div>
-            <form action="/create" method="POST">
-                @csrf
-                <div>
-                    <label for="roomname">部屋名</label>
-                    <input type="text" name="room[roomname]" id="roomname" placeholder="部屋名" value="{{ old('room.roomname') }}" />
-                    @if ($errors->has('room.roomname'))
-                        <p class="roomname__error" style="color:red">{{ $errors->first('room.roomname') }}</p>
-                    @endif
-                </div>
-                <div>
-                    <label for="roompass">部屋パスワード</label>
-                    <!-- パスワードの桁数はデータベースと相談。requestファイルで指定。 -->
-                    <input type="password" name="room[roompass]" id="roompass" value="{{ old('room.roompass') }}" />
-                    @if ($errors->has('room.roompass'))
-                        <p class="roompass__error" style="color:red">{{ $errors->first('room.roompass') }}</p>
-                    @endif
-                    @if ($errors->has('error'))
-                        <div class="alert alert-danger">
-                            {{ $errors->first('error') }}
-                        </div>
-                    @endif
-                </div>
-                <div>
-                    <label for="gamepass">ゲーム内パスワード(※後で設定可能)</label>
-                    <input type="password" name="room[gamepass]" id="gamepass"  value="{{ old('room.gamepass') }}" />
-                    @if ($errors->has('room.gamepass'))
-                        <p class="gamepass__error" style="color:red">{{ $errors->first('room.gamepass') }}</p>
-                    @endif
-                </div>
-                <div>
-                    <label for="number_of_winners">抽選人数(※後で設定可能)</label>
-                    <input type="number" name="room[number_of_winners]" id="number_of_winners" min="1" max="99" value="{{ old('room.number_of_winners') }}" />
-                    @if ($errors->has('room.number_of_winners'))
-                        <p class="number_of_winners__error" style="color:red">{{ $errors->first('room.number_of_winners') }}</p>
-                    @endif
-                </div>
-                <div>
-                    <label for="max_win">最大当選回数(※後で設定可能)</label>
-                    <input type="number" name="room[max_win]" id="max_win" min="1" max="99" value="{{ old('room.max_win') }}" />
-                    @if ($errors->has('room.max_win'))
-                        <p class="max_win__error" style="color:red">{{ $errors->first('room.max_win') }}</p>
-                    @endif
-                </div>
-                <div><input type="submit" value="作成する" /></div>
-            </form>       
+        <div class="body">
+            <div class="form-container">
+                <form action="/create" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label for="roomname">部屋名</label>
+                        <input type="text" name="room[roomname]" id="roomname" value="{{ old('room.roomname') }}" />
+                        @if ($errors->has('room.roomname'))
+                            <p class="error">{{ $errors->first('room.roomname') }}</p>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="roompass">部屋パスワード</label>
+                        <!-- パスワードの桁数はデータベースと相談。requestファイルで指定。 -->
+                        <input type="password" name="room[roompass]" id="roompass" value="{{ old('room.roompass') }}" />
+                        @if ($errors->has('room.roompass'))
+                            <p class="error">{{ $errors->first('room.roompass') }}</p>
+                        @endif
+                        @if ($errors->has('error'))
+                            <div class="alert alert-danger">
+                                {{ $errors->first('error') }}
+                            </div>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="gamepass">ゲーム内パスワード(※後で設定可能)</label>
+                        <input type="password" name="room[gamepass]" id="gamepass" placeholder="空白OK" value="{{ old('room.gamepass') }}" />
+                        @if ($errors->has('room.gamepass'))
+                            <p class="error">{{ $errors->first('room.gamepass') }}</p>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="number_of_winners">抽選人数(※後で設定可能)</label>
+                        <input type="number" name="room[number_of_winners]" id="number_of_winners" min="1" max="99" placeholder="空白OK" value="{{ old('room.number_of_winners') }}" />
+                        @if ($errors->has('room.number_of_winners'))
+                            <p class="error">{{ $errors->first('room.number_of_winners') }}</p>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="max_win">最大当選回数(※後で設定可能)</label>
+                        <input type="number" name="room[max_win]" id="max_win" min="1" max="99" placeholder="空白OK" value="{{ old('room.max_win') }}" />
+                        @if ($errors->has('room.max_win'))
+                            <p class="error">{{ $errors->first('room.max_win') }}</p>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="作成する" class="button"/>
+                    </div>
+                </form>       
+            </div>
         </div>
     </body>
 </x-app-layout>
--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -3,42 +3,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Build</title>
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>RE:SCHOOL</title>
+    <link href="https://fonts.googleapis.com/css?family=DotGothic16&family=Nunito:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/start.css') }}">
 </head>
 <x-app-layout>
-<body>
-    <h1>{{ $room->roomname }}</h1>
-    <div>
-       接続数 {{ $accessNumber }} 名
-    </div>
-    <form action="/start/{{ $room->id }}" method="POST">
-        @csrf
-        @method('PUT')
-        <div>
-            <label for="gamepass">ゲーム内パスワード</label>
-            <input type="password" name="room[gamepass]" id="gamepass" value="{{ $room->gamepass }}"/>
-            @if ($errors->has('room.gamepass'))
-                <p class="gamepass__error" style="color:red">{{ $errors->first('room.gamepass') }}</p>
-            @endif
-            
-        </div>
-        <div>
-            <label for="number_of_winners">抽選人数</label>
-            <input type="number" name="room[number_of_winners]" id="number_of_winners" min="1" max="99" value="{{ $room->number_of_winners }}" />
-            @if ($errors->has('room.number_of_winners'))
-                <p class="number_of_winners__error" style="color:red">{{ $errors->first('room.number_of_winners') }}</p>
-            @endif
-        </div>
-        <div>
-            <label for="max_win">最大当選回数</label>
-            <input type="number" name="room[max_win]" id="max_win" min="1" max="99" value="{{ $room->max_win }}" />
-            @if ($errors->has('room.max_win'))
-                <p class="max_win__error" style="color:red">{{ $errors->first('room.max_win') }}</p>
-            @endif
-        </div>
-        <div><input type="submit" value="マッチ開始" /></div>
-    </form>  
-</body>
+    <body>
+        <div class="body">
+            <div class="form-container">
+                <div class="room-info">
+                    <span class="room-name">{{ $room->roomname }}</span>
+                    <span class="room-status">を運営中...</span>
+                </div>
+                <div class="access-count">
+                    接続数: {{ $accessNumber }} 名
+                </div>
+                <form action="/start/{{ $room->id }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <div class="form-group">
+                        <label for="gamepass">ゲーム内パスワード</label>
+                        <input type="password" name="room[gamepass]" id="gamepass" value="{{ $room->gamepass }}"/>
+                        @if ($errors->has('room.gamepass'))
+                            <p class="error">{{ $errors->first('room.gamepass') }}</p>
+                        @endif                    
+                    </div>
+                    <div class="form-group">
+                        <label for="number_of_winners">抽選人数</label>
+                        <input type="number" name="room[number_of_winners]" id="number_of_winners" min="1" max="99" value="{{ $room->number_of_winners }}" />
+                        @if ($errors->has('room.number_of_winners'))
+                            <p class="error">{{ $errors->first('room.number_of_winners') }}</p>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="max_win">最大当選回数</label>
+                        <input type="number" name="room[max_win]" id="max_win" min="1" max="99" value="{{ $room->max_win }}" />
+                        @if ($errors->has('room.max_win'))
+                            <p class="error">{{ $errors->first('room.max_win') }}</p>
+                        @endif
+                    </div>
+                    <div class="from-group">
+                        <input type="submit" value="マッチ開始" class="button"/>
+                    </div>
+                </form> 
+            </div>
+        </div> 
+    </body>
 </x-app-layout>
 </html>
